671c2f7bb7e36b5d0f559ae8aab5fc7097640fd1
abdoi94 <abdoi94.iq@gmail.com>
2019-11-20 15:28:44 +0300

    build gapps for derpfest

--- aosip_raphael.mk
+++ aosip_raphael.mk
@@ -6,8 +6,15 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common AOSiP stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_MINIMAL_APPS := false
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
@@ -15,4 +22,7 @@ PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := aosip_raphael
 
+BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
